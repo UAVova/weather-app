8ed968fc3cc66ae3f578f2b2e4c62fc45e6c5f37
UAVova <dev.volodymyr@gmail.com>
2018-01-13 23:12:32 +0200

    Added temp level for each city

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Line } from 'react-chartjs';
 
 class WeatherList extends Component {
+  convertDate(date) {
+    return new Date(date).toLocaleTimeString([],
+      {
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+      }
+    );
+  }
+
   renderWeather(data) {
+    const temps = data.list.map(weather => weather.main.temp);
+    // We're using blank label because of lack of space
+    const labels = Array(temps.length).fill('');
+    const chartData = {
+      datasets: [{
+        data: temps
+      }],
+      labels,
+    }
+
     return (
       <tr key={data.city.id}>
         <td>
           {data.city.name}
+        </td>
+        <td>
+          <Line data={chartData} width="300" height="250"/>
         </td>
       </tr>
     )
@@ -32,7 +57,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-          {this.props.weather.map(this.renderWeather)}
+          {this.props.weather.map(data => this.renderWeather(data))}
         </tbody>
       </table>
     )
